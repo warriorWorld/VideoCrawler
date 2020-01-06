40eb10c5566aa814ab5869c4021386c8a95f388c
warriorWorld <772192594@qq.com>
2020-01-06 11:26:54 +0800

    下载

---
+++ app/src/main/java/com/insightsurfface/videocrawler/app/App.java
@@ -0,0 +1,48 @@
+package com.insightsurfface.videocrawler.app;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.Build;
+import android.os.StrictMode;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+/**
+ * Created by Administrator on 2017/7/19.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initImageLoader(getApplicationContext());
+        dealFileUriExposedException();
+    }
+
+    private void dealFileUriExposedException() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.setVmPolicy(builder.build());
+        }
+    }
+
+    private void initImageLoader(Context context) {
+        // This configuration tuning is custom. You can tune every option, you may tune some of them,
+        // or you can create default configuration by
+        //  ImageLoaderConfiguration.createDefault(this);
+        // method.
+        ImageLoaderConfiguration.Builder config = new ImageLoaderConfiguration.Builder(context);
+        config.threadPriority(Thread.NORM_PRIORITY - 2);
+        config.denyCacheImageMultipleSizesInMemory();
+        config.diskCacheFileNameGenerator(new Md5FileNameGenerator());
+        config.diskCacheSize(50 * 1024 * 1024); // 50 MiB
+        config.tasksProcessingOrder(QueueProcessingType.LIFO);
+        config.writeDebugLogs(); // Remove for release app
+
+        // Initialize ImageLoader with configuration.
+        ImageLoader.getInstance().init(config.build());
+    }
+}
